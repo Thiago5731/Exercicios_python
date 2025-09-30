8d91ad61109c416bc99145ab8dfbbfe50a5730c3
Thiago5731 <thiagov88@outlook.com>
2025-09-30 12:31:03 -0300

    Add files via upload

---
+++ #iterable o que é?.py
@@ -0,0 +1,13 @@
+#iterable o que é? 
+
+#estrutura que vc pode percorrer
+#é uma estrutura com varios dados dentro dela e percorrer essa extrutura que você pode percorrer
+# essa estrutura e fazer um for dento delea e percorre todos os dados
+#exemplo: dicionario, listas, tuplas 
+#se a função me der como resposta um iterable eu posso manipular com um loop
+# 
+# exemplo pratico: 
+produtos = ('tv', 'console', 'mesa')
+for produto in produtos:
+    print(produto)
+
