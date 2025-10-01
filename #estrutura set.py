d108cbb42f4bcc367d52c833de121a3c77087a2d
Thiago5731 <thiagov88@outlook.com>
2025-09-30 21:30:44 -0300

    Add files via upload

---
+++ #estrutura set.py
@@ -0,0 +1,10 @@
+#estrutura set
+#analisa varios valores mas sem valores duplicados. 
+#meu_set = {valor, valor, valor}
+#e random o valor nunca vem igual. 
+
+#posso pegar um lista e completar com: 
+
+lista = ['1', '2', '2', '3']
+lista2 = set(lista)
+print(lista2)
