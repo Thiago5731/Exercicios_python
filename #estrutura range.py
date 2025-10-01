45a35b2b7cee2d595f97551a827b576eb8f53ada
Thiago5731 <thiagov88@outlook.com>
2025-09-30 21:27:33 -0300

    Add files via upload

---
+++ #estrutura range.py
@@ -0,0 +1,22 @@
+#estrutura range
+
+#estrutura: 
+#range(tamanho) ou range(inicio, fim) ou range(inicio, fim, passp)
+
+#tamanho: 
+
+for i in range(5):
+    print(i)
+
+#inicio e fim: 
+print(range(1, 10))
+
+for i in range(1, 10):
+    print(i)
+
+#range com passo: 
+#esse conta de 0 a 100 pulando de 10 em 10
+print(range(0, 10,2))
+
+for i in range(0, 1000, 10):
+    print(i)
