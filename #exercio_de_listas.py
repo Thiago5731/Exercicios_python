be61bcc4e3afbd39f85620d7a1aafd5a3387bf98
Thiago5731 <thiagov88@outlook.com>
2025-10-07 13:54:54 -0300

    Update #exercio_de_listas.py

--- #exercio_de_listas.py
+++ #exercio_de_listas.py
@@ -10,6 +10,13 @@ lista = []
 for c in range(5):
     lista.append(int(input('Digite o um numero: ')))
 print(f'Sua lista ficou assim: {lista}')
-POS = int(input('Digite um indice: '))
-print(f'O indice selecionado equivale ao valor: {lista[POS]}')
+while True:
+    POS = int(input('Digite um indice de 1 a 5: '))
+    Valor = POS - 1 
+    if POS < 1 or POS > 5: 
+        print('Por favor escolha uam opção valida!')
+    else:
+        break
+    
+print(f'O indice selecionado equivale ao valor: {lista[Valor]}')
 print(f'O primeiro valor da lista é: {lista[0]} e o ultimo é: {lista[4]}')
